9e11d38389280c94bc08aa9031e27de6956ece21
htlspenger2014 <htl-spenger-2014@gmx.at>
2013-09-19 20:03:38 +0200

    Fehler weg!

--- HalloGit/src/test/Bus.java
+++ HalloGit/src/test/Bus.java
@@ -1,5 +1,22 @@
 package test;
 
-public class Bus {
 
+
+public class Bus {
+	private int anzahl;
+
+	public int getAnzahl() {
+		return anzahl;
+	}
+
+	public void setAnzahl(int anzahl) {
+		this.anzahl = anzahl;
+	}
+
+	public Bus(int anzahl) {
+		super();
+		this.anzahl = anzahl;
+		
+	}
+	
 }
